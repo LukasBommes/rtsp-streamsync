c2aa3f3e9f7d30299959b0c946143b6f2c634f57
LukasBommes <mail@lukasbommes.de>
2019-08-02 12:21:12 +0800

    added free for allocated memory

--- src/frame_packet_deque.hpp
+++ src/frame_packet_deque.hpp
@@ -6,12 +6,13 @@
 
 /** Thread-safe ring buffer for SSFramePacket
 *
-*  A ring buffer implementation based on std::deque. Pop() retrieves a new item
-*  from the deque front (oldest item) and blocks if the deque is empty. If no
-*  maxsize is specified, push() never blocks and inserts a new item in the back
-*  of the deque. If maxsize is specified the deque grows until this size is
-*  reached. Further calls to push() will remove the oldest item from the deque
-*  and only then insert the new item, keeping the size of the deque constant.
+*  A ring buffer implementation based on std::deque. Pop() retrieves a new
+*  frame_packet from the deque front (oldest frame_packet) and blocks if the
+*  deque is empty. If no maxsize is specified, push() never blocks and inserts a
+*  new frame_packet in the back of the deque. If maxsize is specified the deque
+*  grows until this size is reached. Further calls to push() will remove the
+*  oldest frame_packet from the deque and only then insert the new frame_packet,
+*  keeping the size of the deque constant.
 *
 *   @param maxsize If <= 0 (default) do not limit the size of the deque. If > 0
 *       allow the deque to reach at most this size.
@@ -28,37 +29,47 @@ public:
         {
           this->cond_.wait(mlock);
         }
-        auto item = this->deque_.front();
+        SSFramePacket frame_packet = this->deque_.front();
         this->deque_.pop_front();
-        return item;
+        return frame_packet;
     }
 
-    void pop(SSFramePacket& item) {
+    void pop(SSFramePacket& frame_packet) {
         std::unique_lock<std::mutex> mlock(this->mutex_);
         while (this->deque_.empty())
         {
           this->cond_.wait(mlock);
         }
-        item = this->deque_.front();
+        frame_packet = this->deque_.front();
         this->deque_.pop_front();
     }
 
-    void push(const SSFramePacket& item) {
+    void push(const SSFramePacket& frame_packet) {
         std::unique_lock<std::mutex> mlock(this->mutex_);
         if (this->maxsize > 0 && (this->deque_.size() == this->maxsize)) { // deque is full
+            SSFramePacket frame_packet = this->deque_.front();
+            for (std::size_t cap_id = 0; cap_id < frame_packet.size(); cap_id++) {
+                free((*frame_packet[cap_id]).frame);
+                free((*frame_packet[cap_id]).motion_vectors);
+            }
             this->deque_.pop_front();
         }
-        this->deque_.push_back(item);
+        this->deque_.push_back(frame_packet);
         mlock.unlock();
         this->cond_.notify_one();
     }
 
-    void push(SSFramePacket&& item) {
+    void push(SSFramePacket&& frame_packet) {
         std::unique_lock<std::mutex> mlock(this->mutex_);
         if (this->maxsize > 0 && (this->deque_.size() == this->maxsize)) { // deque is full
+            SSFramePacket frame_packet = this->deque_.front();
+            for (std::size_t cap_id = 0; cap_id < frame_packet.size(); cap_id++) {
+                free((*frame_packet[cap_id]).frame);
+                free((*frame_packet[cap_id]).motion_vectors);
+            }
             this->deque_.pop_front();
         }
-        this->deque_.push_back(std::move(item));
+        this->deque_.push_back(std::move(frame_packet));
         mlock.unlock();
         this->cond_.notify_one();
     }
